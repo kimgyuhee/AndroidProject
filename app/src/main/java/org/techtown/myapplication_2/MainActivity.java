e07bee0b99b5ab59841e768251ff080ddb5c8aff
hyerin6 <0419hyerin@gmail.com>
2018-09-17 20:22:58 +0900

    layoutVersion_01

---
+++ app/src/main/java/org/techtown/myapplication_2/MainActivity.java
@@ -0,0 +1,23 @@
+package org.techtown.myapplication_2;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Onclick(View v){
+
+    }
+
+
+
+
+
+}
